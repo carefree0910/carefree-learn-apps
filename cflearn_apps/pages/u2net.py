452a4836d196aedf267e878cc06cd4a64ab49bf3
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2021-08-11 14:30:10 +0800

    Update u2net.py

--- cflearn_apps/pages/u2net.py
+++ cflearn_apps/pages/u2net.py
@@ -2,6 +2,7 @@ import numpy as np
 import streamlit as st
 
 from PIL import Image
+from typing import Any
 from requests import Response
 from skimage.transform import resize
 from cflearn_deploy.toolkit import cutout
@@ -11,9 +12,9 @@ from cflearn_deploy.api_utils import post_img_arr
 
 
 @st.cache
-def get_rgba_response(src: np.ndarray, smooth: int, tight: float) -> Response:
+def get_rgba_response(src: np.ndarray, **kwargs: Any) -> Response:
     with st.spinner("Generating RGBA image..."):
-        return post_img_arr(src, uri="/cv/sod", smooth=smooth, tight=tight)
+        return post_img_arr(src, uri="/cv/sod", **kwargs)
 
 
 def app() -> None:
@@ -25,6 +26,7 @@ def app() -> None:
         thresh = st.sidebar.slider("Threshold", min_value=0.0, max_value=1.0, value=0.5)
     smooth = st.sidebar.slider("Smooth", min_value=0, max_value=20, value=0)
     tight = st.sidebar.slider("Tight", min_value=0.0, max_value=1.0, value=0.9)
+    model = st.sidebar.text_input("Model Name", "sod")
 
     uploaded_file = st.file_uploader("Please upload your file")
     if uploaded_file is not None:
@@ -36,7 +38,12 @@ def app() -> None:
         normalized_img = img_arr.astype(np.float32) / 255.0
         resized_img = resize(img_arr, (320, 320), mode="constant")
         resized_img = resized_img.astype(np.float32)
-        rgba_response = get_rgba_response(resized_img, smooth=smooth, tight=tight)
+        rgba_response = get_rgba_response(
+            resized_img,
+            model_name=model,
+            smooth=smooth,
+            tight=tight,
+        )
         if not rgba_response.ok:
             st.markdown(f"**Failed to get alpha mask! ({rgba_response.reason})**")
         else:
