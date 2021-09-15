ef88ddb3f9a9bccbafbdfec430361ab38878da0e
carefree0910 <syameimaru.saki@gmail.com>
2021-09-15 17:57:20 +0800

    Update sod.py

--- cflearn_apps/pages/sod.py
+++ cflearn_apps/pages/sod.py
@@ -30,11 +30,11 @@ def app() -> None:
     if uploaded_file is not None:
         col1, col2 = st.columns(2)
         image = Image.open(uploaded_file).convert("RGB")
-        image.thumbnail((320, 320), Image.ANTIALIAS)
+        original_shape = image.size
+        original_normalized_img = np.array(image).astype(np.float32) / 255.0
         col1.image(image, caption="Uploaded Image")
-        img_arr = np.array(image)
-        normalized_img = img_arr.astype(np.float32) / 255.0
-        resized_img = resize(img_arr, (320, 320), mode="constant")
+        image.thumbnail((320, 320), Image.LANCZOS)
+        resized_img = resize(np.array(image), (320, 320), mode="constant")
         resized_img = resized_img.astype(np.float32)
         rgba_response = get_rgba_response(
             resized_img,
@@ -48,6 +48,9 @@ def app() -> None:
             alpha = bytes_to_np(rgba_response.content, mode="RGBA")[..., -1]
             if thresh is not None:
                 alpha = (alpha > thresh).astype(np.float32)
-            alpha, rgba = cutout(normalized_img, alpha, smooth, tight)
+            alpha_img = Image.fromarray(to_uint8(alpha))
+            alpha = np.array(alpha_img.resize(original_shape, Image.LANCZOS))
+            alpha = alpha.astype(np.float32) / 255.0
+            alpha, rgba = cutout(original_normalized_img, alpha, smooth, tight)
             col2.image(to_uint8(alpha), caption="Mask")
             st.image(rgba, caption="RGBA")
