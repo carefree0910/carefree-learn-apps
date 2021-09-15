535e41db3842d15b096b2577cc93a38cfcd29007
carefree0910 <syameimaru.saki@gmail.com>
2021-09-15 17:57:18 +0800

    Update clf.py

--- cflearn_apps/pages/clf.py
+++ cflearn_apps/pages/clf.py
@@ -23,17 +23,15 @@ def app() -> None:
     uploaded_file = st.file_uploader("Please upload your file")
     if uploaded_file is not None:
         image = Image.open(uploaded_file).convert("RGB")
-        image.thumbnail((224, 224), Image.ANTIALIAS)
         col1, col2 = st.columns(2)
         col1.image(image, caption="Uploaded Image")
-        img_arr = np.array(image)
-        resized_img = resize(img_arr, (224, 224), mode="constant")
+        image.thumbnail((224, 224), Image.ANTIALIAS)
+        resized_img = resize(np.array(image), (224, 224), mode="constant")
         resized_img = resized_img.astype(np.float32)
         prob_response = get_prob_response(resized_img, onnx_name=model)
         if not prob_response.ok:
             st.markdown(f"**Failed to get probabilities! ({prob_response.reason})**")
         else:
-            rs = json.loads(prob_response.content)
-            probabilities = np.array(rs["probabilities"], dtype=np.float32).tolist()
+            probabilities = json.loads(prob_response.content)["probabilities"]
             top_indices = np.argsort(probabilities).tolist()[::-1][:top_k]
             col2.json({str(i): probabilities[i] for i in top_indices})
