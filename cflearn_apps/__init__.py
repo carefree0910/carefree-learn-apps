c30640d7d02abcb606e8db812a87640821b2852d
carefree0910 <syameimaru.saki@gmail.com>
2021-09-18 15:30:10 +0800

    Renamed `style_gan` -> `generation` and Enhanced it

--- cflearn_apps/__init__.py
+++ cflearn_apps/__init__.py
@@ -3,6 +3,6 @@ from .pages import sod
 from .pages import cbir
 from .pages import tbir
 from .pages import adain
-from .pages import style_gan
+from .pages import generation
 from .pages import color_extraction
 from .multipage import MultiPage
--- cflearn_apps/pages/generation.py
+++ cflearn_apps/pages/generation.py
@@ -12,8 +12,9 @@ def get_rgb_response(**kwargs: Any) -> Response:
 
 
 def app() -> None:
-    model = st.sidebar.radio(
-        "Model Name",
+    model = st.sidebar.text_input("Model Name", "style_gan")
+    sub_type = st.sidebar.radio(
+        "Model Sub Type",
         [
             "afhqcat",
             "afhqdog",
@@ -23,18 +24,19 @@ def app() -> None:
         ],
         index=0,
     )
+    model_name = f"{model}.{sub_type}"
     mapping = {
-        "afhqcat": "cat",
-        "afhqdog": "dog",
-        "afhqwild": "animal",
-        "ffhq": "person",
-        "metfaces": "art piece",
+        "style_gan.afhqcat": "cat",
+        "style_gan.afhqdog": "dog",
+        "style_gan.afhqwild": "animal",
+        "style_gan.ffhq": "person",
+        "style_gan.metfaces": "art piece",
     }
-    rgb_response = get_rgb_response(onnx_name=model)
+    rgb_response = get_rgb_response(onnx_name=model_name)
     if not rgb_response.ok:
         st.markdown(f"**Failed to generate image! ({rgb_response.reason})**")
     else:
         col1, col2 = st.columns(2)
-        col1.subheader(f"This {mapping[model]} does not exist!")
+        col1.subheader(f"This {mapping[model_name]} does not exist!")
         col2.button("Refresh!")
         st.image(bytes_to_np(rgb_response.content, mode="RGB"))
