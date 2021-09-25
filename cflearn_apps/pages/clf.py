ef8436818e5aab610d85505bdd34b96cb0ffa5e2
carefree0910 <syameimaru.saki@gmail.com>
2021-09-25 20:25:35 +0800

    Enhanced `clf`

--- cflearn_apps/pages/clf.py
+++ cflearn_apps/pages/clf.py
@@ -1,3 +1,4 @@
+import os
 import json
 
 import numpy as np
@@ -9,6 +10,10 @@ from typing import Any
 from requests import Response
 from skimage.transform import resize
 from cflearn_deploy.api_utils import post_img_arr
+from cflearn_deploy.constants import LABEL_KEY
+from cflearn_deploy.constants import PREDICTIONS_KEY
+
+from ..constants import META_FOLDER
 
 
 @st.cache
@@ -19,7 +24,21 @@ def get_prob_response(src: np.ndarray, **kwargs: Any) -> Response:
 
 def app() -> None:
     top_k = st.sidebar.slider("Top K", min_value=1, max_value=20, value=5)
-    model = st.sidebar.text_input("Model Name", "cct")
+    model = st.sidebar.text_input("Model Name", "default")
+    task = st.sidebar.radio(
+        "Task",
+        [
+            "products",
+            "fonts",
+        ],
+        index=0,
+    )
+    if model == "default":
+        if task == "products":
+            model = "cct_large"
+        elif task == "fonts":
+            model = "cct"
+    model_name = f"{model}.{task}"
 
     uploaded_file = st.file_uploader("Please upload your file")
     if uploaded_file is not None:
@@ -29,32 +48,52 @@ def app() -> None:
         image.thumbnail((224, 224), Image.ANTIALIAS)
         resized_img = resize(np.array(image), (224, 224), mode="constant")
         resized_img = resized_img.astype(np.float32)
-        prob_response = get_prob_response(resized_img, onnx_name=model)
+        kwargs = {}
+        if task == "fonts":
+            kwargs["gray"] = True
+            kwargs["no_transform"] = True
+        prob_response = get_prob_response(resized_img, onnx_name=model_name, **kwargs)
         if not prob_response.ok:
             st.markdown(f"**Failed to get probabilities! ({prob_response.reason})**")
         else:
-            kwargs = dict(figsize=(12, 7), subplot_kw=dict(aspect="equal"), dpi=80)
-            fig, ax = plt.subplots(**kwargs)
             probabilities = json.loads(prob_response.content)["probabilities"]
-            top_indices = np.argsort(probabilities).tolist()[::-1][:top_k]
-            top_probabilities = [probabilities[i] for i in top_indices]
-            top_probabilities.append(1.0 - sum(top_probabilities))
-            wedges, texts, auto_texts = ax.pie(
-                top_probabilities,
-                autopct=lambda value: f"{value:.1f}%",
-                textprops=dict(color="w"),
-                colors=plt.cm.Dark2.colors,
-                startangle=140,
-                normalize=False,
-            )
-            categories = [str(i) for i in top_indices] + ["else"]
-            ax.legend(
-                wedges,
-                categories,
-                title="Class",
-                loc="center left",
-                bbox_to_anchor=(1, 0, 0.5, 1),
-            )
-            plt.setp(auto_texts, size=10, weight=700)
-            ax.set_title("Pie Chart")
-            col2.pyplot(fig)
+            meta_folder = os.path.join(META_FOLDER, task)
+            for k, v in probabilities.items():
+                # fetch
+                if k == PREDICTIONS_KEY:
+                    k = "main"
+                mapping_file = f"idx2{k if k != 'main' else f'{LABEL_KEY}'}.json"
+                mapping_path = os.path.join(meta_folder, mapping_file)
+                if not os.path.isfile(mapping_path):
+                    mapping = None
+                else:
+                    with open(mapping_path, "r") as f:
+                        mapping = json.load(f)
+                top_indices = np.argsort(v).tolist()[::-1][:top_k]
+                top_probabilities = [v[i] for i in top_indices]
+                top_probabilities.append(1.0 - sum(top_probabilities))
+                # draw
+                kwargs = dict(figsize=(12, 7), subplot_kw=dict(aspect="equal"), dpi=80)
+                fig, ax = plt.subplots(**kwargs)
+                wedges, texts, auto_texts = ax.pie(
+                    top_probabilities,
+                    autopct=lambda value: f"{value:.1f}%",
+                    textprops=dict(color="w"),
+                    colors=plt.cm.Dark2.colors,
+                    startangle=140,
+                    normalize=False,
+                )
+                categories = [str(i) for i in top_indices]
+                if mapping is not None:
+                    categories = list(map(mapping.get, categories))
+                categories.append("else")
+                ax.legend(
+                    wedges,
+                    categories,
+                    title="Class",
+                    loc="center left",
+                    bbox_to_anchor=(1, 0, 0.5, 1),
+                )
+                plt.setp(auto_texts, size=10, weight=700)
+                ax.set_title(f"Pie Chart for '{k}'")
+                col2.pyplot(fig)
