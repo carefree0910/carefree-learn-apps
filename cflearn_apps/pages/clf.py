7ec4170348ab75775e001ef2eed69c939858a32e
carefree0910 <syameimaru.saki@gmail.com>
2021-09-26 10:43:40 +0800

    Update clf.py

--- cflearn_apps/pages/clf.py
+++ cflearn_apps/pages/clf.py
@@ -24,7 +24,7 @@ def get_prob_response(src: np.ndarray, **kwargs: Any) -> Response:
 
 def app() -> None:
     top_k = st.sidebar.slider("Top K", min_value=1, max_value=20, value=5)
-    model = st.sidebar.text_input("Model Name", "default")
+    model = st.sidebar.text_input("Model Name", "clf")
     task = st.sidebar.radio(
         "Task",
         [
@@ -33,11 +33,6 @@ def app() -> None:
         ],
         index=0,
     )
-    if model == "default":
-        if task == "products":
-            model = "cct_large"
-        elif task == "fonts":
-            model = "cct"
     model_name = f"{model}.{task}"
 
     uploaded_file = st.file_uploader("Please upload your file")
