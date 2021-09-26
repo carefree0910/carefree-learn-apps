058b4f460b76b0e4bdf2b1bc98fab552eaed0a16
carefree0910 <syameimaru.saki@gmail.com>
2021-09-26 14:12:22 +0800

    Update utils.py

--- cflearn_apps/pages/utils.py
+++ cflearn_apps/pages/utils.py
@@ -7,6 +7,7 @@ from PIL import Image
 from PIL import ImageFile
 from typing import Any
 from typing import Callable
+from typing import Optional
 from requests import Response
 
 from ..constants import IMAGES_FOLDER
@@ -36,6 +37,9 @@ def image_retrieval(
     top_k: int,
     num_probe: int,
     columns: tuple,
+    gray: bool = False,
+    no_transform: bool = False,
+    caption_callback: Optional[Callable[[str], str]] = None,
 ) -> None:
     indices_response = get_indices_response(
         uri,
@@ -45,6 +49,8 @@ def image_retrieval(
         onnx_name=model,
         top_k=top_k,
         nprobe=num_probe,
+        gray=gray,
+        no_transform=no_transform,
     )
     if not indices_response.ok:
         reason = indices_response.reason
@@ -54,13 +60,17 @@ def image_retrieval(
         files, distances = rs["files"], rs["distances"]
         img_folder = os.path.join(IMAGES_FOLDER, src_folder)
         for k, k_files in files.items():
-            columns[0].markdown("---")
-            columns[1].markdown(
-                f"<h3 style='text-align: center'> {k} </h3>",
-                unsafe_allow_html=True,
-            )
-            columns[2].markdown("---")
+            if len(files) > 1:
+                columns[0].markdown("---")
+                columns[1].markdown(
+                    f"<h3 style='text-align: center'> {k} </h3>",
+                    unsafe_allow_html=True,
+                )
+                columns[2].markdown("---")
             k_distances = distances[k]
             for i, (file, distance) in enumerate(zip(k_files, k_distances)):
                 img = Image.open(os.path.join(img_folder, file)).convert("RGB")
-                columns[i % 3].image(img, caption=f"{distance:8.6f}")
+                caption = f"{distance:8.6f}"
+                if caption_callback is not None:
+                    caption = f"{caption}, {caption_callback(file)}"
+                columns[i % 3].image(img, caption=caption)
